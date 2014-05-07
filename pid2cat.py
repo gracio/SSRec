6c65942223b8888513848a4660f92a4a83d35abe
Grace T. Huang <graceworkhuang@gmail.com>
2014-05-06 18:15:17 -0700

    rewrote pid2cat for updated tag to identifier matching

--- pid2cat.py
+++ pid2cat.py
@@ -38,88 +38,117 @@ def pid2cat(PID, levelN = -1):
 	# grab tags for PID
 	# first hard match against men, women, others
 	# next, within the root level category, find best word match
-	tag = []
-	category_node_id = []
-	identifier = []
+
 	score = []
 	exit_flag = 'ok'
 	level2parent = []
-	score = []
-	parent_id = []
-	depth = []
+
 	conn = pymysql.connect(host='127.0.0.1', port=3306, user='shopstyle_view', passwd='born2run', db='shopstyle_prod')  
 	cur = conn.cursor(pymysql.cursors.DictCursor)
-	success = cur.execute("select product.name, product.brand_id, product.price, product.status, category_node.tag, category_node.identifier, category_node.category_node_id, category_node.parent_id, category_node.depth from product_tag join category_node on product_tag.category_tag = category_node.tag join product on product_tag.product_id = product.product_id and product.uber_id = " + PID) 
-	if success:
+
+	# get product tags
+	success_tag = cur.execute("select product_tag.category_tag, product.brand_id, product.price, product.status from product_tag join product on product.product_id = product_tag.product_id and product.uber_id = " + PID)
+	if success_tag:
 		retrieved = cur.fetchall()
+		tag_set = []
 		status = retrieved[0]['status']
-		name = retrieved[0]['name']
 		brand = retrieved[0]['brand_id']
 		price = retrieved[0]['price']
 		for j in retrieved:
-			tag.append(j['tag'])
-			category_node_id.append(j['category_node_id'])
-			identifier.append(j['identifier'])
-			parent_id.append(j['parent_id'])
-			depth.append(j['depth'])
-		if not tag or not identifier or not category_node_id:
-			exit_flag = '! no tags, identifier, or category_node_id available'
-			#print 'exit_flag:', exit_flag
-			cur.close()
-			if levelN < 0:
-				return PID, None,None,None,None,None, None, None, None, None, None, exit_flag
-			else:
-				return None, None
-		tag_set = set(tag)
+			tag_set.append(j['category_tag'])
+		tag_set = set(tag_set)
 		tag_set = [i.lower() for i in tag_set]
-		for j in identifier:
-			score.append(tanimoto(tag_set, j.split()))
-		if max(score) > 0:
-			matches_id = [idx for idx, i in enumerate(score) if i == max(score) and parent_id[idx]]
-			#print matches_id, parent_id[idx]
-			if matches_id:
-				match_id = matches_id[0]
-				cat = category_node_id[match_id]
-				identifier = identifier[match_id]
-				if levelN > 0:
-					levelNparent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,levelN)
-				else:
-					level1parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,1)
-					level2parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,2)
-					level3parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,3)
-			else:
-				matches_id = [idx for idx, i in enumerate(score) if i == max(score)]
-				match_id = matches_id[0]
-				cat = category_node_id[match_id]
-				identifier = identifier[match_id]
-				level3parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,3)
-				if levelN == 3:
-					levelNparent = level3parent
-				else:
-					level2_idx = [idx for idx, i in enumerate(depth) if i == 2]
-					max_level2_score = max([score[i] for i in level2_idx])
-					level2parent_idx = [i for i in level2_idx if score[i] == max_level2_score and parent_id[i]]
-					if level2parent_idx:
-						level2parent = category_node_id[level2parent_idx[0]]
-						level1parent = parent_id[level2parent_idx[0]]
-					else:
-						level2parent = cat
-						level1parent = cat
-					if levelN == 2:
-						levelNparent = level2parent
-					else:
-						levelNparent = level1parent
-		cur.close()
-		tags = '-'.join(tag_set)
 	else:
-		exit_flag = '! unavailable from database'
+		exit_flag = '! tag_set unavailable from database'
 		#print 'exit_flag:', exit_flag
 		cur.close()
 		if levelN < 0:
 			return PID, None,None,None,None,None, None, None, None, None, None, exit_flag
 		else:
 			return None, None
+
+	# get candidate category_nodes matching the tags
+	success_catnode = cur.execute("select identifier, category_node_id, parent_id, depth from category_node where deleted = 00000000 ") 
+	if success_catnode:
+		retrieved = cur.fetchall()
+		category_node_id = []
+		identifier = []
+		parent_id = []
+		depth = []
+		for j in retrieved:
+			category_node_id.append(j['category_node_id'])
+			identifier.append(j['identifier'])
+			parent_id.append(j['parent_id'])
+			depth.append(j['depth'])
+	else:
+		exit_flag = '! catnode unavailable from database'
+		#print 'exit_flag:', exit_flag
+		cur.close()
+		if levelN < 0:
+			return PID, None,None,None,None,None, None, None, None, None, None, exit_flag
+		else:
+			return None, None
+
+	# if info from SSmysql is empty, exit
+	if not tag_set or not identifier or not category_node_id:
+		exit_flag = '! no tags, identifier, or category_node_id available'
+		#print 'exit_flag:', exit_flag
+		cur.close()
+		if levelN < 0:
+			return PID, None,None,None,None,None, None, None, None, None, None, exit_flag
+		else:
+			return None, None
+
+	# calculate matching score between product tags and candidate category identifiers
+	for j in identifier:
+		score.append(tanimoto(tag_set, j.split()))
+
+	if max(score) > 0:
+		matches_id = [idx for idx, i in enumerate(score) if i == max(score) and parent_id[idx]]
+		#print matches_id, parent_id[idx]
+		if matches_id:
+			match_id = matches_id[0]
+			cat = category_node_id[match_id]
+			identifier = identifier[match_id]
+			if levelN > 0:
+				levelNparent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,levelN)
+			else:
+				level1parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,1)
+				level2parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,2)
+				level3parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,3)
+		else:
+			matches_id = [idx for idx, i in enumerate(score) if i == max(score)]
+			match_id = matches_id[0]
+			cat = category_node_id[match_id]
+			identifier = identifier[match_id]
+			level3parent, levelNcategoryName, ownLevel, ownName = levelNcategory(cat,3)
+			if levelN == 3:
+				levelNparent = level3parent
+			else:
+				level2_idx = [idx for idx, i in enumerate(depth) if i == 2]
+				max_level2_score = max([score[i] for i in level2_idx])
+				level2parent_idx = [i for i in level2_idx if score[i] == max_level2_score and parent_id[i]]
+				if level2parent_idx:
+					level2parent = category_node_id[level2parent_idx[0]]
+					level1parent = parent_id[level2parent_idx[0]]
+				else:
+					level2parent = cat
+					level1parent = cat
+				if levelN == 2:
+					levelNparent = level2parent
+				else:
+					levelNparent = level1parent
+	cur.close()
+	tags = '-'.join(tag_set)
+
 	if levelN < 0:
 		return PID, cat, level3parent, level2parent, level1parent, ownLevel, brand, price, identifier, status, tags, exit_flag
 	else:
 		return brand, levelNparent
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
